afff522d2fa4cb6117d54f18593564808a58a15d
Louis <qingyanjiu@126.com>
2017-12-31 11:36:01 +0800

    added spring cloud support

--- src/main/java/priv/louis/live/controller/IndexController.kt
+++ src/main/java/priv/louis/live/controller/IndexController.kt
@@ -1,18 +1,36 @@
 package priv.louis.live.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller("/")
 class IndexController {
 
-    @RequestMapping("")
-    open fun index() : String{
-        return "index"
+    @Value("\${live.server.snapshoturl}")
+    private val profile: String = ""
+
+    @Value("\${live.client.title}")
+    private val title: String = ""
+
+    @RequestMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("title", title);
+        return "index";
     }
 
-    @RequestMapping("hello")
+    @RequestMapping("/login")
+    fun login(model: Model): String {
+        model.addAttribute("title", title);
+        model.addAttribute("login", "login");
+        return "index";
+    }
+
+    @RequestMapping("/profile")
     @ResponseBody
-    fun hello() : String = "Hello"
+    fun profile(model: Model): String {
+        return this.profile;
+    }
 }
